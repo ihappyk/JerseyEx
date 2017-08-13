902694981a0cce256513e0083141f077dc785bd4
ihappyk <santhoshks2312@gmail.com>
2017-08-13 22:19:30 +0530

    commit message

--- src/main/java/com/mkyong/rest/HelloWorldService.java
+++ src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -1,21 +1,31 @@
 package com.mkyong.rest;
- 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
- 
-@Path("/hello")
+
+@Path("/")
 public class HelloWorldService {
- 
+
+	@GET
+	@Path("ping")
+	public Response getMsg() {
+
+		String output = "Jersey Says : Ping Works Buddy";
+
+		return Response.status(200).entity(output).build();
+
+	}
+
 	@GET
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
- 
+
 		String output = "Jersey say : " + msg;
- 
+
 		return Response.status(200).entity(output).build();
- 
+
 	}
- 
+
 }
